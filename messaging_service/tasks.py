bd0239b59da8feaac3e7f23a36c760969f405ac5
Manish <manishgkasera@gmail.com>
2016-10-15 19:03:07 +0530

    exponential backoff with max_retries

--- messaging_service/tasks.py
+++ messaging_service/tasks.py
@@ -2,6 +2,11 @@ from celery import Celery
 
 app = Celery('tasks', broker='redis://localhost', backend='redis://localhost')
 
-@app.task
-def add(x, y):
-    return x+y
+
+@app.task(bind=True, max_retries=10)
+def add(self, x, y):
+    try:
+        raise ValueError('A very specific bad thing happened')
+    except ValueError as exc:
+        self.retry(exc=exc, countdown=2 ** self.request.retries)
+    return x + y
